9542ed83308600504b4abf6fea0e39e8beef99aa
Yuriy <yuriy.senchkovskiy@gmail.com>
2022-10-16 11:47:55 +0300

    Initial commit

---
+++ inheritance-01/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace inheritance_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+        }
+    }
+ 
+    class ConsoleLogWritter
+    {
+        public virtual void WriteError(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+ 
+    class FileLogWritter
+    {
+        public virtual void WriteError(string message)
+        {
+            File.WriteAllText("log.txt", message);
+        }
+    }
+ 
+    class SecureConsoleLogWritter : ConsoleLogWritter
+    {
+        public override void WriteError(string message)
+        {
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
+            {
+                base.WriteError(message);
+            }
+        }
+    }
+}
